0fc3c5d196c623d8f47d3295b5f4efbb73ec4344
zhdanov <zhdanov09zh@gmail.com>
2017-11-24 12:14:53 +0300

    initial

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'mysql2'
+gem 'puma', '~> 3.0'
+gem 'rails', '~> 5.0.6'
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'factory_girl_rails', '~> 4.8'
+  gem 'faker', '~> 1.8'
+end
+
+group :test do
+  gem 'rspec-rails', '~> 3.6'
+end
+
+group :development do
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Permissions
+  class Application < Rails::Application
+    config.generators do |g|
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        model_spec: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+    end
+  end
+end
